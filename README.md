0afaae0fee8ef8e74a30e00e00a14fa0d7323d52
francisco caudillo perez <fcaudilloperez1971@gmail.com>
2020-08-31 08:52:49 -0500

    Add method post

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # PrinterTpvBelleza
+curl --header "Content-Type: application/json"   --request POST   --data '{"username":"xyz","password":"xyz"}'   http://localhost:5000/print
+
+
  pip install -U flask-cors
  pip install Flask
--- printer_web.py
+++ printer_web.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/print')
+@app.route('/print', methods=['POST'])
 def hello_world():
+    req_data = request.get_json()
+    encabezado = req_data['username']
+    print encabezado
     return {"saludo":"Hello, World!"}
 
 if __name__ == '__main__':
